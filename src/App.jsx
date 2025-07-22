dfe9b4d3973c7529ef11548344b88f710dcebdc9
alvaro.junqueira <alvaro.alf2@gmail.com>
2025-07-21 21:50:48 -0300

    feat: add link to Simple To-do project in App component

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ function App() {
       <h2>Everything starts with a first step.</h2>
       <p>I learned react a few years ago. I haven't used it so much since my focus has been backend with Python. I decided it's time to master it. So, here are some little projects I coded in my journey to master Reactjs. Small projects are great for developing muscle memory! 🦾</p>
       <a href="/">001 - First Step - 2025-07-21</a>
+      <a href="https://002-simple-todo.vercel.app/">002 - Simple To-do - 2025-07-21</a>
     </>
   )
 }
