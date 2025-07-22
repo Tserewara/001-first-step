0eb8970ec7f8b50c233739bbc8893bf7cddf18b2
alvaro.junqueira <alvaro.alf2@gmail.com>
2025-07-21 21:53:41 -0300

    refactor: simplify project links rendering in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,18 @@
+const projects = [
+  {
+    id: 1,
+    title: "001 - First Step",
+    date: "2025-07-21",
+    link: "/"
+  },
+  {
+    id: 2,
+    title: "002 - Simple To-do",
+    date: "2025-07-21",
+    link: "https://002-simple-todo.vercel.app/"
+  }
+];
+
 function App() {
 
   return (
@@ -5,8 +20,11 @@ function App() {
     <h1>React Workout</h1>
       <h2>Everything starts with a first step.</h2>
       <p>I learned react a few years ago. I haven't used it so much since my focus has been backend with Python. I decided it's time to master it. So, here are some little projects I coded in my journey to master Reactjs. Small projects are great for developing muscle memory! 🦾</p>
-      <a href="/">001 - First Step - 2025-07-21</a>
-      <a href="https://002-simple-todo.vercel.app/">002 - Simple To-do - 2025-07-21</a>
+      {projects.map((project) => (
+        <div key={project.id}>
+          <a href={project.link}>{project.title} - {project.date}</a>
+        </div>
+      ))}
     </>
   )
 }
